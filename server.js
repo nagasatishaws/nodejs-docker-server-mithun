fee1ac4bbf8a48f74471af7b5038c9221fc0c621
nagasatishaws <64299813+nagasatishaws@users.noreply.github.com>
2021-04-06 23:23:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('This is docker-kubernetes based Nodejs Application');
+  res.send('This is minikube-kuber based Nodejs Application');
 });
 
 app.listen(PORT, HOST);
